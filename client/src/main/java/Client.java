84f7c237a51f4017322ebdac3a81fd45d3f97196
Tim Göller <tim.goeller@posteo.de>
2019-11-13 11:00:04 +0100

    Add default initialization for client aswell

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,3 +1,45 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import static util.RabbitMQUtils.CreateDefaultExchanges;
+import static util.RabbitMQUtils.CreateDefaultQueues;
+
 public class Client {
 
+    private Channel channel;
+
+    Client(@NotNull String rabbitMQHost, @NotNull String rabbitMQUser, @NotNull String rabbitMQPass, @NotNull Integer rabbitMQPort) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(rabbitMQHost);
+        factory.setPort(rabbitMQPort);
+        factory.setUsername(rabbitMQUser);
+        factory.setPassword(rabbitMQPass);
+
+        InitializeRabbitMQConnection(factory);
+    }
+
+    private void InitializeRabbitMQConnection(@NotNull ConnectionFactory factory) {
+        try {
+            System.out.println("Creating connection...");
+            Connection connection = factory.newConnection();
+            System.out.println("Connection created successfully");
+
+            System.out.println("Creating channel...");
+            channel = connection.createChannel();
+            System.out.println("Channel created successfully with number " + channel.getChannelNumber());
+
+            CreateDefaultExchanges(channel);
+            CreateDefaultQueues(channel);
+        }
+        catch (TimeoutException e) {
+            System.out.println("Timeout while trying to connect to the RabbitMQ server");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
