f6d98cbf8f220a54c3ea2b4dbb067eee3f27de97
Tim Göller <tim.goeller@posteo.de>
2019-11-13 11:05:19 +0100

    Add .idea folder to project, to share run configuration

--- client/src/main/java/Program.java
+++ client/src/main/java/Program.java
@@ -1,9 +1,11 @@
 import util.Config;
+import util.PrimeUtil;
 
 public class Program {
 
     public static void main(String[] argv) {
         Config config = Config.readConfigFromCLIArgs(argv);
+        Client host = new Client(config.hostIP, config.username, config.password, config.port);
     }
 
 }
